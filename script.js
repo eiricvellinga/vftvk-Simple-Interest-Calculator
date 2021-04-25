a3970456b054117e59815ec3ecb158ca055e8db2
eiricvellinga <83020206+eiricvellinga@users.noreply.github.com>
2021-04-25 14:25:07 +0200

    Update script.js
    , verwijderd

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="\<br\>If you deposit <mark>"+principal+
     "</mark>,\<br\>at an interest rate of <mark>"+rate+
-    "%</mark>,\<br\>You will receive an amount of <mark>"+interest+
+    "%</mark>\<br\>You will receive an amount of <mark>"+interest+
     "</mark>,\<br\>in the year "+year+"\<br\>"
         
 }
